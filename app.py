b346d278ebc87b9b64073e04d1fb4d27d2a1b603
Swatkats <34835897+Swatkats13@users.noreply.github.com>
2024-11-08 02:06:00 -0500

    Final Changes

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def hypothesis_test(X, Y, parameter='slope', null_value=0, test_type='greater', 
     elif test_type == 'less':
         p_value = np.mean([sim <= observed_value for sim in simulated_values])
     else:  # 'not equal'
-        p_value = np.mean([abs(sim - observed_value) >= abs(observed_value) for sim in simulated_values])
+        p_value = np.mean([abs(sim - null_value) >= abs(null_value - observed_value) for sim in simulated_values])
 
     return observed_value, simulated_values, p_value
 
